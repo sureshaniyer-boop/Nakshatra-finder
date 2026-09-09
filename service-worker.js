f42422378fdfbf90222fc1adef8d1019e10c0395
sureshaniyer-boop <sureshan.iyer@gmail.com>
2026-09-10 06:01:16 +0800

    Refresh offline cache for corrected Mudakku temple lookup

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shri-ugra-mahalakshmy-astrology-v12";
+const CACHE_NAME = "shri-ugra-mahalakshmy-astrology-v13";
 const APP_SHELL = ["./", "index.html", "mudakku.html", "yogi-avayogi.html", "nakshatra-details.html", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (event) => {
